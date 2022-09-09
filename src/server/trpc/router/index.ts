fb557d6617be0ef516630ad34e3dcb5489679df7
Leo Kling <leo.kling.info@gmail.com>
2022-09-09 17:24:59 +0200

    init

---
+++ src/server/trpc/router/index.ts
@@ -0,0 +1,12 @@
+// src/server/trpc/router/index.ts
+import { t } from "../trpc";
+import { groupRouter } from "./group";
+import { authRouter } from "./auth";
+
+export const appRouter = t.router({
+  example: groupRouter,
+  auth: authRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
